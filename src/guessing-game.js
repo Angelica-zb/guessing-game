f10646d1086f65ff3db9b989304ea0b3a05d4e1d
Angelica-zb <angelika.zaborovskaya@gmail.com>
2022-08-17 02:06:15 +0300

    fix

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.n = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.n = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.n;
     }
 
     greater() {
-
+        this.min = this.n;
     }
 }
 
